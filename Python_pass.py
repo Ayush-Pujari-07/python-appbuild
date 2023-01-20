1a6c7307f96b3763a7d4e5e57d693286d8224e50
Ayush <ayush08.pujari@gmail.com>
2023-01-20 16:29:26 +0530

    2nd commit

--- Python_pass.py
+++ Python_pass.py
@@ -1,9 +1,37 @@
-password = input("enter the password: ")
+def check_passord():
+    password = input("Enter the password: ")
 
-result = []
+    result = []
+
+    if len(password) >= 8:
+        result.append(True)
+    else:
+        result.append(False)
+
+    # Check if it has digit
+    digit = False
+    for i in password:
+        if i.isdigit():
+            digit = True
+
+    result.append(digit)
+
+    # Check if it has uppercase
+    uppercase = False
+    for i in password:
+        if i.isupper():
+            uppercase = True
+
+    result.append(uppercase)
+
+    # Check if it has all
+    if all(result):
+        print("Strong Password!!")
+    else:
+        print("Weak Password!!")
 
 
-if len(password) >= 8:
-    result.append(True)
-else:
-    result.append(False)
+disct = {"apple": "tstu", "car": "asda", "asdf": "asdaweq", "asdooaf": 112, "open": 123, "pass": 1243, "pole": "pipe"}
+
+for i in disct:
+    print(disct[i])
